cd44d8cd8707664f67d847d9d16262a6c3dd9d42
Valentina <Valentina@DESKTOP-VV1TMQV>
2020-11-28 13:15:42 -0400

    primer commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart'; as http;
+import 'package:http/http.dart'; as; http;
 import 'dart:async';
-import 'dart:convert';
+import 'dart:convert'
 
 void main() => runApp(RecuperacionValentina());
 
